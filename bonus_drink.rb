ab5f8230f7ca141b398de93d61f1dc359087e3da
tsutomu katsube <tsuto.igawa@gmail.com>
2015-09-12 08:36:35 +0900

    Add valid argument case

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,7 +1,14 @@
 class BonusDrink
+  PRESENT_PER = 3
+
   def self.total_count_for(amount)
     if amount < 0
       raise(ArgumentError, 'must be 0 or a positive integer')
     end
+    if amount == 0
+      return 0
+    end
+
+    amount + (amount - 1) / (PRESENT_PER - 1)
   end
 end
